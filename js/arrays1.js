27e73ea1d6370e628478682068de4569aebcd76e
shiruiz2208 <gise.13.ruiz@gmail.com>
2023-11-02 01:45:30 -0300

    agregamos ejercicio2

---
+++ js/arrays1.js
@@ -0,0 +1,31 @@
+/*1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+Output:
+Enero
+Febrero
+Marzo
+Abril
+Mayo
+Junio
+Julio
+Agosto
+Septiembre
+Octubre 
+Noviembre
+Diciembre*/
+
+let meses =['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ]; 
+
+document.write(`<p> ${meses[0]}</p>`);
+document.write(`<p> ${meses[1]}</p>`);
+document.write(`<p> ${meses[2]}</p>`);
+document.write(`<p> ${meses[3]}</p>`);
+document.write(`<p> ${meses[4]}</p>`);
+document.write(`<p> ${meses[5]}</p>`);
+document.write(`<p> ${meses[6]}
+</p>`);
+document.write(`<p> ${meses[7]}</p>`);
+document.write(`<p> ${meses[8]}</p>`);
+document.write(`<p> ${meses[9]}</p>`);
+document.write(`<p> ${meses[10]}</p>`);
+document.write(`<p> ${meses[11]}</p>`);
---
+++ js/arrays2.js
@@ -0,0 +1,13 @@
+/*2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+Mostrar la longitud del arreglo.
+Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+Añade en última posición la ciudad de París.
+Escribe por pantalla el elemento que ocupa la segunda posición.
+Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'*/
+
+
+let ciudades = ['Nueva York', 'Estados Unidos', 'Barcelona', 'España','Tokio', 'Japón', 'Londres', 'Reino Unido', 'Roma', 'Italia', 'Pekín', 'China', 'Río de Janeiro', 'Brasi', 'Ámsterdam','Países Bajos', 'Sídney', 'Australia', 'El Cairo', 'Egipto']
+
+console.log= parseInt(prompt('Ingrese el nombre de la ciudad'));
+console.log(ciudades);
